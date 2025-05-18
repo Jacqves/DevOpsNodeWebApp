be3709914a735f799d0eec2fba43d9a121c72816
Jacqves <jack.bruegger@hotmail.com>
2025-05-18 21:14:41 +0200

    Cloud Deploy Render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('(-: Hello FS2025 DevOps Course! Heute ist es Sonntag');
+  res.send('(-: Hello FS2025 DevOps Course! Heute ist es Sonntag und wir deployen auf render.com');
 });
 
 app.listen(PORT, HOST);
